fc7e8295b6aeaa49d45391fc647cd7dd94904643
dreean <s.andrea.storci@gmail.com>
2024-12-20 22:47:55 +0100

    added current-eng

--- andreastorci.it/src/app/sections/current-eng.tsx
+++ andreastorci.it/src/app/sections/current-eng.tsx
@@ -6,8 +6,25 @@
 
 export default function CurrentEng() {
     return (
-        <div className="w-full mt-[100px] px-[300px] h-[200px]">
-            
-        </div>
+        <section className="current-eng">
+            <div className="flex flex-row">
+                <div className="w-[50%] h-[100px] m-[10px]">
+                    <h2>
+                        Front <span>end</span>
+                    </h2>
+                    <p>
+                    Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
+                    </p>
+                </div>
+                <div className="w-[50%] m-[10px]">
+                    <h2>
+                        Back <span>end</span>
+                    </h2>
+                    <p>
+                        Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
+                    </p>
+                </div>
+            </div>
+        </section>
     )
 }
